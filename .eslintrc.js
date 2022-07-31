151beb1f300a03754dca53eda92e3165d6777ff6
Nasty <nasia2000@yandex.ru>
2022-07-31 17:58:20 +0500

    finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+    'max-len': 'off',
+    'linebreak-style': 'off',
+    'eol-last': 'off'
+  }
+}
